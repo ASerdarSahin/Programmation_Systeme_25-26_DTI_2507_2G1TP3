f7b85df5f6f30c4f5c5b3bf267877799b6f77094
ASerdarSahin <71930822+ASerdarSahin@users.noreply.github.com>
2025-12-08 09:26:55 +0100

    Add files via upload

---
+++ Q1/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "question_1.h"
+
+int main(){
+	display_welcome_prompt();
+	display_regular_prompt();
+}
---
+++ Q1/question_1.h
@@ -0,0 +1,8 @@
+#ifndef QUESTION_1_H
+#define QUESTION_1_H
+
+void display_welcome_prompt();
+#define WELCOME_PROMPT "Welcome to ENSEA tiny shell!\n"
+void display_regular_prompt();
+#define NORMAL_PROMPT "Type 'exit' to quit.\n"
+#endif
