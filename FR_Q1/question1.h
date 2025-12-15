84d4aba010bc5eaa79932296426a0d52315a7254
pathe1041 <111251472+pathe1041@users.noreply.github.com>
2025-12-15 06:05:05 -0500

    Add files via upload

---
+++ FR_Q1/question1.h
@@ -0,0 +1,9 @@
+#ifndef QUESTION1_H
+#define QUESTION1_H
+#define WELCOME_PROMPT "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
+
+
+void print_prompt(void);
+void display_welcome_prompt(void);
+
+#endif
