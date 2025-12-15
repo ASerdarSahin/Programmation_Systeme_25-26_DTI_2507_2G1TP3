1765830dcd88c2102b93fcd20353945a41fddfb4
ASerdarSahin <71930822+ASerdarSahin@users.noreply.github.com>
2025-12-15 10:32:36 +0100

    Delete Q4/temp directory

--- Q4/temp/question_4.c
+++
@@ -1,4 +0,0 @@
-#include "question_4.h"
-
-int last_exit_code = -1;
-int last_signal = -1;
--- Q4/temp/question_4.h
+++
@@ -1,7 +0,0 @@
-#ifndef QUESTION_4_H
-#define QUESTION_4_H
-
-extern int last_exit_code;
-extern int last_signal;
-
-#endif
