8d9754f351733cd48e67974bcf43fbd12aa82eab
ASerdarSahin <71930822+ASerdarSahin@users.noreply.github.com>
2025-12-15 09:18:47 +0100

    temp-delete

---
+++ Q4/temp/question_1.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <string.h>
+#include "question_1.h"
+#include "question_4.h"
+#include <stdio.h>
+void display_welcome_prompt(){
+	write(STDOUT_FILENO, WELCOME_PROMPT, strlen(WELCOME_PROMPT));
+}
+
+void display_regular_prompt(){
+	char prompt[PROMPT_BUFFER_SIZE];
+	int len;
+	if (last_exit_code != 1) {
+	  len = snprintf(prompt, PROMPT_BUFFER_SIZE,
+			"Enseah [exit:%d] %%\n", last_exit_code);
+	}
+	else if (last_signal != -1) {
+	  len = snprintf(prompt, PROMPT_BUFFER_SIZE,
+			"Enseash [sign:%d] %%\n", last_signal);
+	}
+	else {
+	  len = snprintf(prompt, PROMPT_BUFFER_SIZE,
+			"Enseash %%\n");
+	}
+
+	write(STDOUT_FILENO, prompt, len);
+}
